20d69f9555c9526b9354c797849d3dff47cb3c17
LimDae94 <piay801@naver.com>
2023-12-20 22:15:55 +0900

    #16 - 게시글 뷰 기본적인 틀 구성
    컨트롤러, 뷰, 테스트까지 한 번에 모두 적용했습니다.

--- bootBoard/src/main/java/gc/bootboard/controller/ArticleController.java
+++ bootBoard/src/main/java/gc/bootboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package gc.bootboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,4 +26,11 @@ public class ArticleController {
         return "articles/index";
     }
 
+    @GetMapping("{articleId}")
+    public String articles(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("articles", "article"); // TODO: 구현할 때, "article" 대신 실제 데이터를 작성해야 합니다. 
+        map.addAttribute("articlesComments", List.of());
+
+        return "articles/index";
+    }
 }
